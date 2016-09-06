0279024548de934eff00dfa407a43ca7d234002a
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-06 15:37:43 +0200

    Create Constants.java

---
+++ day1/Constants.java
@@ -0,0 +1,15 @@
+package com.zetcode;
+
+public class ConstantsEx {
+
+    public static void main(String[] args) {
+        
+        final int WIDTH = 100;
+        final int HEIGHT= 150;
+        int var = 40;
+
+        var = 50;
+        
+        //WIDTH = 110;        
+    }
+}
