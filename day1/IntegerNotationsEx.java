de56810cf5df26368e58fc6ad25035fa4fa35063
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-06 16:19:09 +0200

    Create IntegerNotationsEx.java

---
+++ day1/IntegerNotationsEx.java
@@ -0,0 +1,17 @@
+package com.zetcode;
+
+public class IntegerNotationsEx {
+
+    public static void main(String[] args) {
+        
+        int n1 = 31;
+        int n2 = 0x31;
+        int n3 = 031;
+        int n4 = 0b1001;
+        
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);        
+    }
+}
