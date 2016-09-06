d00d58c7acef455c7dabffeac12d60eb535b5028
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-06 14:38:33 +0200

    Create CaseSensitiveEx.java

---
+++ day1/CaseSensitiveEx.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+public class CaseSensitiveEx {
+
+    public static void main(String[] args) {
+
+        int age = 12;
+        int Age = 18;
+        
+        System.out.println(age);
+        System.out.println(Age);
+    }
+}
