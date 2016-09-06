3b7a09f89301fab746c1f14ded0939f7a2815c81
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-06 16:17:04 +0200

    Create UsingUnderscoresEx.java

---
+++ day1/UsingUnderscoresEx.java
@@ -0,0 +1,12 @@
+package com.zetcode;
+
+public class UsingUnderscoresEx {
+    
+    public static void main(String[] args) {
+        
+        long a = 23482345629L;
+        long b = 23_482_345_629L;
+
+        System.out.println(a == b);                                                    
+    }    
+}
