76fcdceede48c0cc235eddebe9935d58de71bf2f
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-06 15:06:08 +0200

    Create OperatorsEx.java

---
+++ day1/OperatorsEx.java
@@ -0,0 +1,16 @@
+package com.zetcode;
+
+public class OperatorsEx {
+
+    public static void main(String[] args) {
+        
+        int price = 13;
+        int quantity = 4;
+        
+        int sum = price * quantity;
+        
+        System.out.println(sum);
+        
+        System.out.println("Sunny" + " day");
+    }
+}
