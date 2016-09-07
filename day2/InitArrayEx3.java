5c9a31035ec24a011b333380fb74c4427beb65cb
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 19:01:43 +0200

    Create InitArrayEx3.java

---
+++ day2/InitArrayEx3.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+import java.util.Arrays;
+
+public class InitArrayEx3 {
+
+    public static void main(String[] args) {
+
+        int[] a = { 2, 4, 5, 6, 7, 3, 2 };
+
+        System.out.println(Arrays.toString(a));
+    }
+}
