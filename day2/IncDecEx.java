3afae178721c08c6a3fbac3e420c3e1a0f2a5d61
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 20:49:45 +0200

    Create IncDecEx.java

---
+++ day2/IncDecEx.java
@@ -0,0 +1,17 @@
+package com.zetcode;
+
+public class IncDecEx {
+
+    public static void main(String[] args) {
+
+        int x = 6;
+
+        x++;
+        x++;
+
+        System.out.println(x);
+
+        x--;
+        System.out.println(x);
+    }
+}
