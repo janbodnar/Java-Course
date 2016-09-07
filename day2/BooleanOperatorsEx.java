0d34598de6c25faf397ad7d70f6de709141ff8d4
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 20:53:43 +0200

    Create BooleanOperatorsEx.java

---
+++ day2/BooleanOperatorsEx.java
@@ -0,0 +1,18 @@
+package com.zetcode;
+
+public class BooleanOperatorsEx {
+
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 8;
+
+        System.out.println(x == y);
+        System.out.println(y > x);
+
+        if (y > x) {
+            
+            System.out.println("y is greater than x");
+        }
+    }
+}
