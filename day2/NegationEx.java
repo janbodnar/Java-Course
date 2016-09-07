e18de8807f8021ffc3ccd8e003db184d976cee27
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 20:55:43 +0200

    Create NegationEx.java

---
+++ day2/NegationEx.java
@@ -0,0 +1,11 @@
+package com.zetcode;
+
+public class NegationEx {
+
+    public static void main(String[] args) {
+        
+        System.out.println(! true);
+        System.out.println(! false);
+        System.out.println(! (4 < 3));    
+    }
+}
