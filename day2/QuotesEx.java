1df297e90721d7807f02c4227b000fe5a305b526
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 17:34:12 +0200

    Create QuotesEx.java

---
+++ day2/QuotesEx.java
@@ -0,0 +1,10 @@
+package com.zetcode;
+
+public class QuotesEx {
+
+    public static void main(String[] args) {
+        
+        System.out.println("There are may stars");
+        System.out.println("He said: \"Which one are you looking at?\"");
+    }
+}
