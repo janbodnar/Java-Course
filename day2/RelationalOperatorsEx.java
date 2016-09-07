f8d2ef43780000d8478c9c3333cd3aa4d46e4269
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 20:57:54 +0200

    Create RelationalOperatorsEx.java

---
+++ day2/RelationalOperatorsEx.java
@@ -0,0 +1,12 @@
+package com.zetcode;
+
+public class RelationalOperatorsEx {
+
+    public static void main(String[] args) {
+        
+        System.out.println(3 < 4);
+        System.out.println(3 == 4);
+        System.out.println(4 >= 3);
+        System.out.println(4 != 3);
+    }
+}
