99d67b572d07899d6bca81f96a67776b40ee8c51
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 20:57:23 +0200

    Create CompoundOperatorsEx.java

---
+++ day2/CompoundOperatorsEx.java
@@ -0,0 +1,18 @@
+package com.zetcode;
+
+public class CompoundOperatorsEx {
+
+    public static void main(String[] args) {
+        
+        int a = 1;
+        a = a + 1;
+ 
+        System.out.println(a); 
+        
+        a += 5;
+        System.out.println(a);    
+        
+        a *= 3;
+        System.out.println(a);    
+    }
+}
