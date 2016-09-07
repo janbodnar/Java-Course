a5db34f77f0e558e854a0c0d20140330e729ba67
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 20:51:50 +0200

    Create DivisionEx.java

---
+++ day2/DivisionEx.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+public class DivisionEx {
+
+    public static void main(String[] args) {
+        
+        int c = 5 / 2;
+        System.out.println(c);
+
+        double d = 5 / 2.0;
+        System.out.println(d);    
+    }
+}
