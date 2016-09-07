58e648ad6a023578811eb955c166dec97dcd4ba8
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 07:26:29 +0200

    Create SimpleGuiEx.java

---
+++ day1/SimpleGuiEx.java
@@ -0,0 +1,10 @@
+package com.zetcode;
+
+import javax.swing.JOptionPane;
+
+public class SimpleGuiEx {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Swing GUI");
+    }
+}
