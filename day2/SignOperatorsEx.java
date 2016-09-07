2059059d48ff5a23ff47c59e5f1373db9a71553b
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-07 20:47:24 +0200

    Create SignOperatorsEx.java

---
+++ day2/SignOperatorsEx.java
@@ -0,0 +1,11 @@
+package com.zetcode;
+
+public class SignOperatorsEx {
+
+    public static void main(String[] args) {
+        
+        System.out.println(2);
+        System.out.println(+2);
+        System.out.println(-2);
+    }
+}
