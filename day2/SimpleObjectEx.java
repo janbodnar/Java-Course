cd919b3f80380192e4669394601aade8e117ae67
Jan Bodnar <jan.bodnar@gmail.com>
2016-09-08 07:13:03 +0200

    Create SimpleObjectEx.java

---
+++ day2/SimpleObjectEx.java
@@ -0,0 +1,12 @@
+package com.zetcode;
+
+class Being {}
+
+public class SimpleObjectEx {
+
+    public static void main(String[] args) {
+        
+        Being b = new Being();
+        System.out.println(b);
+    }
+}
