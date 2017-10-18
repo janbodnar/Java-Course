339033ca01fd5326e23669d18b5f439b409c11eb
Jan Bodnar <jan.bodnar@gmail.com>
2017-10-18 23:29:45 +0200

    Create JavaCopyFileEx.java

---
+++ examples/JavaCopyFileEx.java
@@ -0,0 +1,25 @@
+package com.zetcode;
+
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class JavaCopyFileEx {
+
+    public static void main(String[] args) throws IOException {
+
+        Path fromPath = Paths.get("/home/janbodnar/tmp/myfile.txt");
+        Path toPath = Paths.get("/home/janbodnar/tmp/myfile2.txt");
+        
+        CopyOption[] options = new CopyOption[]{
+            StandardCopyOption.REPLACE_EXISTING,
+            StandardCopyOption.COPY_ATTRIBUTES
+        };
+        
+        Files.copy(fromPath, toPath, options);
+
+    }
+}
