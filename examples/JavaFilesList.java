2291cfc78618b1aae553f342ca0eaaba42eeda3f
Jan Bodnar <jan.bodnar@gmail.com>
2017-12-12 18:01:37 +0100

    Update JavaFilesList.java

--- examples/JavaFilesList.java
+++ examples/JavaFilesList.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+// non-recursive
+
 public class JavaFilesList {
 
     public static void main(String[] args) throws IOException {
